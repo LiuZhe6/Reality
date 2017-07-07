69349aa3bef373996b637616457fee6178da7590
LiuZhe <miuiclub@qq.com>
2017-07-07 20:31:11 +0800

    VPN Java

---
+++ Vpn/app/src/main/java/com/vpn/MainFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.vpn;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by coder on 17-6-30.
+ */
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter{
+
+    private ArrayList<Fragment> fragments;
+
+    public MainFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public MainFragmentPagerAdapter(FragmentManager fm,ArrayList<Fragment> fragments){
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return super.getItemPosition(object);
+    }
+}
---
+++ Vpn/app/src/main/java/com/vpn/utils/MyVpnThread.java
@@ -0,0 +1,8 @@
+package com.vpn.utils;
+
+/**
+ * Created by coder on 17-7-5.
+ */
+public class MyVpnThread extends Thread{
+
+}
