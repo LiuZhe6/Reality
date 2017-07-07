f240d12ba9a4a289abe0539022fa09ea1fb4d2ca
LiuZhe <miuiclub@qq.com>
2017-07-07 19:41:42 +0800

    VPN Java

---
+++ Vpn/app/src/main/java/com/vpn/OptionFragment.java
@@ -0,0 +1,27 @@
+package com.vpn;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class OptionFragment extends Fragment {
+
+
+    public OptionFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.option,container,false);
+        return v;
+    }
+
+}
---
+++ Vpn/app/src/main/java/com/vpn/PurchaseFragment.java
@@ -0,0 +1,27 @@
+package com.vpn;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class PurchaseFragment extends Fragment {
+
+
+    public PurchaseFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.purchase,container,false);
+        return v;
+    }
+
+}
